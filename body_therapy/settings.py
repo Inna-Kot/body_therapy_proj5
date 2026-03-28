3d0cbe7b1b4fefeb0a4d2d8d2acfe88de02fc8d7
Inna-Kot <kotkovets.dev@outlook.ie>
2026-03-28 13:51:09 +0000

    fix: set debug to false and fix static url

--- body_therapy/settings.py
+++ body_therapy/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Allow Render domain and local environments
 ALLOWED_HOSTS = ['body-therapy-proj5.onrender.com', 'localhost', '127.0.0.1']
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
