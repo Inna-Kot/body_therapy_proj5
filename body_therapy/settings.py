7f045a624878886437b566cbe5dc548e4c701e3f
Inna-Kot <kotkovets.dev@outlook.ie>
2026-04-03 13:41:08 +0100

    Restore CSRF_TRUSTED_ORIGINS for Render admin access

--- body_therapy/settings.py
+++ body_therapy/settings.py
@@ -21,7 +21,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 # Allow Render domain and local environments
 ALLOWED_HOSTS = ['body-therapy-proj5.onrender.com', 'localhost', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://body-therapy-proj5.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
