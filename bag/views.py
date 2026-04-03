8e07c2f18747a97c79706e863c8d906415d243ed
Inna-Kot <kotkovets.dev@outlook.ie>
2026-04-03 16:53:09 +0100

    Add success messages to bag views for user feedback

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from services.models import Service
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -7,6 +9,8 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified service to the shopping bag """
     
+    # Отримуємо об'єкт сервісу, щоб знати його назву для красивого повідомлення
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     
@@ -14,8 +18,12 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        # Повідомлення про оновлення кількості
+        messages.success(request, f'Updated {service.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        # Повідомлення про нове додавання
+        messages.success(request, f'Added {service.name} to your bag')
 
     request.session['bag'] = bag
     
