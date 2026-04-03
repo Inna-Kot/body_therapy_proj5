5d2e9fabeb2e68d1da3fa08a545e53c6b483c7e7
Inna-Kot <kotkovets.dev@outlook.ie>
2026-04-03 13:21:31 +0100

    Register Service and Category models in admin panel

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Service, Category
 
-# Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'duration_minutes',
+        'image',
+    )
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Category, CategoryAdmin)
