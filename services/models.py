6383f88ea2c255c5925afbbee51cda9b9a78094a
Inna-Kot <kotkovets.dev@outlook.ie>
2026-04-03 17:46:43 +0100

    Add Category model and establish ForeignKey relationship in Service model

--- services/models.py
+++ services/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
+    """ Model for service categories """
     class Meta:
         verbose_name_plural = 'Categories'
         
@@ -15,12 +16,14 @@ class Category(models.Model):
 
 
 class Service(models.Model):
+    """ Model for individual therapy services """
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     duration_minutes = models.IntegerField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
+    
     def __str__(self):
         return self.name
