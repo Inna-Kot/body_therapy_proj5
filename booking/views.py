4a5dbb9e584cb2919aea69889f80d7c5ec62701e
Inna-Kot <kotkovets.dev@outlook.ie>
2026-04-12 19:56:39 +0100

    Setup booking model, admin and protected book_now view

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from services.models import Service
 
-# Create your views here.
+@login_required
+def book_now(request, service_id):
+    """
+    View to handle the booking process. 
+    Redirects to login if user is not authenticated.
+    """
+    service = get_object_or_404(Service, id=service_id)
+    
+    # temporary context for booking page, can be expanded with more details as needed
+    context = {
+        'service': service,
+    }
+    
+    return render(request, 'booking/book_now.html', context)
