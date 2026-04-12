bd43631e67defecc6ef35b1e8670aecc47e7d2da
Inna-Kot <kotkovets.dev@outlook.ie>
2026-04-12 14:04:33 +0100

    Add custom fields to Service model and update navigation

--- services/models.py
+++ services/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Category(models.Model):
     """ Model for service categories """
@@ -20,10 +21,25 @@ class Service(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=254, unique=True, null=True, blank=True)
     description = models.TextField()
+    
+    # Custom fields for medical/therapy specificity
+    contraindications = models.TextField(null=True, blank=True)
+    preparation = models.TextField(null=True, blank=True)
+    
     price = models.DecimalField(max_digits=6, decimal_places=2)
     duration_minutes = models.IntegerField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        """ 
+        Override save method to automatically generate 
+        a slug from the service name if not provided.
+        """
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
