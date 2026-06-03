dd853f52f79d028ac71aa68820ab0739d15aead6
Inna-Kot <kotkovets.dev@outlook.ie>
2026-06-03 20:20:36 +0100

    fix(settings): switch Gmail SMTP to SSL port 465 for production compatibility

--- body_therapy/settings.py
+++ body_therapy/settings.py
@@ -155,9 +155,10 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+# Email Settings for Gmail SMTP (Configured for production deployment)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
