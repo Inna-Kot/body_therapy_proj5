e445ce1d509ca2cf2a4bda4b955fbadfaee12f1f
Inna-Kot <kotkovets.dev@outlook.ie>
2026-07-04 13:50:58 +0100

    Use hashed static storage for cache busting

--- body_therapy/settings.py
+++ body_therapy/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_URL = '/media/'
